fde17c516eac948c7313083601f285d32a549d6a
fabien townsend <fabientownsend@gmail.com>
2016-09-28 11:08:50 +0100

    remove travis and coverall installation

--- setup_kata_ruby.sh
+++ setup_kata_ruby.sh
@@ -1,20 +1,8 @@
 echo "create the Gemfile"
 echo "source \"https://rubygems.org\"
 
-gem 'rake'
 gem 'rspec', '~> 3.0'
-gem 'guard-rspec', require: false
-gem 'coveralls', require: false" >  Gemfile
-
-echo "Create the Rakefile"
-echo "require 'rake'
-require 'rspec/core/rake_task'
-
-RSpec::Core::RakeTask.new(:spec) do |t|
-  t.pattern = Dir.glob('spec/**/*_spec.rb')
-end
-
-task :default => :spec" > Rakefile
+gem 'guard-rspec', require: false" >  Gemfile
 
 echo "installation bundle"
 gem install bundle
@@ -24,19 +12,6 @@ bundle install --binstubs
 
 echo "initialisation rspec"
 bin/rspec --init
-
-echo "creation travis file"
-echo "language: ruby
-rvm:
-- 2.3.0
-script:
-- bundle exec rspec" > .travis.yml
-
-echo "require 'coveralls'
-Coveralls.wear!" > test.txt
-cat spec/spec_helper.rb >> test.txt
-cp test.txt spec/spec_helper.rb
-rm test.txt
 
 echo "create class exemple"
 mkdir lib
@@ -48,7 +23,7 @@ end" > lib/example.rb
 
 echo "create test exemple"
 
-echo "require 'spec_helper'
+echo "
 require 'example'
 RSpec.describe Example do
   it \"should execute the test\" do
@@ -58,5 +33,7 @@ RSpec.describe Example do
 end" > spec/example_spec.rb
 
 echo "launch the test"
+
+guard init rspec
 rspec
 guard
